ca72bbfedf9b563f8e76bf221942aba2daf6d7da
JPedroCMartins <jpedro7682@gmail.com>
2026-08-05 22:27:58 -0400

    fix: bug do instance

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+
 from app import create_app, db
 
 app = create_app()
 
 if __name__ == '__main__':
      # Cria o banco se não existir
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
